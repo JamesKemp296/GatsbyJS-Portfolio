ece62324415b49d17d5c90773188ef3281e08b2c
JamesKemp296 <jameskemp2796@yahoo.com>
2019-11-24 20:49:58 -0500

    initial gatsby link setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/about/">Go to page about</Link>
+    <Link to="/skills/">Go to page skills</Link>
+    <Link to="/contact/">Go to page contact</Link>
   </Layout>
 )
 
