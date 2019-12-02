2dc8670519be65d61d2e1c9993ab669c733e5622
James Kemp <jameskemp2796@yahoo.com>
2019-12-01 19:56:08 -0500

    small change to site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `James Kemp`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `James Kemp`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
