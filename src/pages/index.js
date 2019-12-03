ef58520031fd319a4ecc683a620238bdf034e22d
JamesKemp296 <jameskemp2796@yahoo.com>
2019-12-02 21:10:38 -0500

    added images for projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/about/">Go to page about</Link>
-    <Link to="/skills/">Go to page skills</Link>
-    <Link to="/contact/">Go to page contact</Link>
+    <h1>What's up early bird, come back when the site is done!</h1>
   </Layout>
 )
 
