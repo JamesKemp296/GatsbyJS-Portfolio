7a959c212f3878fda440ae9454b6e72f823b0711
JamesKemp296 <jameskemp2796@yahoo.com>
2019-12-10 20:35:30 -0500

    styles to portfolio and about structure created

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import ProjectPrewview from '../components/project-preview'
 import SEO from "../components/seo"
 
+import '../components/layout.css'
 import "./index.css"
 
 const IndexPage = () => {
@@ -33,34 +34,58 @@ const IndexPage = () => {
   return(
     <Layout>
       <SEO title="Portfolio" />
-      <div className="portfolio-hero-wrapper">
-        <div className="portfolio-hero">
-          <h1>Full-Stack Developer</h1>
-          <p>
-            Creating beautiful, highly interactive user interfaces in ReactJS with powerful and robust backends.
-          </p>
+      <div className="container">
+        <div className="portfolio-hero-wrapper">
+          <div className="portfolio-hero">
+            <div className="wiggle-flex">
+              <h1 className="wiggle">F</h1>
+              <h1 className="wiggle">u</h1>
+              <h1 className="wiggle">l</h1>
+              <h1 className="wiggle">l</h1>
+              <h1 className="wiggle">-</h1>
+              <h1 className="wiggle">S</h1>
+              <h1 className="wiggle">t</h1>
+              <h1 className="wiggle">a</h1>
+              <h1 className="wiggle">c</h1>
+              <h1 className="wiggle">k</h1>
+              <h1 className="wiggle"> D</h1>
+              <h1 className="wiggle">e</h1>
+              <h1 className="wiggle">v</h1>
+              <h1 className="wiggle">e</h1>
+              <h1 className="wiggle">l</h1>
+              <h1 className="wiggle">o</h1>
+              <h1 className="wiggle">p</h1>
+              <h1 className="wiggle">e</h1>
+              <h1 className="wiggle">r</h1>
+            </div>
+            <p>
+              Creating beautiful, highly interactive user interfaces in ReactJS with powerful and robust backends.
+            </p>
+          </div>
+        </div>
+        <div id="portfolio-button">
+          <h5 id="portfolio-button-text">VIEW PORTFOLIO</h5>
+          <h4 id="portfolio-button-chevron">&#xFE40;</h4>
         </div>
       </div>
-      <div id="portfolio-button">
-        <h5 id="portfolio-button-text">VIEW PORTFOLIO</h5>
-        <h4 id="portfolio-button-chevron">&#xFE40;</h4>
-      </div>
-      <div className="project-grid">
-        {projects.map(({ node: project }, i) => {
-          const title = project.title
-          const description = project.description
-          const slug = project.slug
-          const imageData = project.image.childImageSharp.fluid
-          return(
-            <ProjectPrewview
-              key={i}
-              title={title}
-              description={description}
-              imageData={imageData}
-              slug={slug}
-            />
-          )
-        })}
+      <div className="container">
+        <div className="project-grid">
+          {projects.map(({ node: project }, i) => {
+            const title = project.title
+            const description = project.description
+            const slug = project.slug
+            const imageData = project.image.childImageSharp.fluid
+            return(
+              <ProjectPrewview
+                key={i}
+                title={title}
+                description={description}
+                imageData={imageData}
+                slug={slug}
+              />
+            )
+          })}
+        </div>
       </div>
     </Layout>
   )
