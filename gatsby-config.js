d74707aa3c59149fccec4d38cb8e4699aa158101
JamesKemp296 <jameskemp2796@yahoo.com>
2019-12-12 22:51:04 -0500

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `minimal-ui`,
+        icon: `public/icons/favicon.ico`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
