0b1a00f55edaebf78c6c298dde602f520f973402
JamesKemp296 <jameskemp2796@yahoo.com>
2019-12-12 22:18:00 -0500

    enhanced styles on project page

--- src/components/project.js
+++ src/components/project.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import SEO from "../components/seo"
 import Image from 'gatsby-image'
+import "./project.css"
 
 const Project = ({ title, description, tech, github, url, imageData }) => (
-  <div className="project">
-    <h1>{title}</h1>
-    <Image fluid={imageData} alt={title}/>
-    <p>{description}</p>
-    <p>Tech used: {tech}</p>
-    <p>
-      <a href={url} target="_blank" rel="noopener noreferrer">View this project online &rarr;</a>
-    </p>
-    <p>
-      <a href={github} target="_blank" rel="noopener noreferrer">View this project on github &rarr;</a>
-    </p>
-    <p>
-      <Link to="/">&larr; back to all projects.</Link>
-    </p>
-  </div>
+  <>
+    <SEO title={title} />
+    <div className="project">
+      <header className="project-header">
+        <h1>{title}</h1>
+        <h2>Subheader will go here, I'll make one</h2>
+      </header>
+      <div className="project-content">
+        <div className="project-container">
+          <Image fluid={imageData} alt={title}/>
+          <p>{description}</p>
+          <p>Tech used: {tech}</p>
+          <p>
+            <a href={url} target="_blank" rel="noopener noreferrer">View this project online</a>
+          </p>
+          <p>
+            <a href={github} target="_blank" rel="noopener noreferrer">View this project on github</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </>
 )
 
 export default Project
