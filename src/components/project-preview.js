6600d8fd1396c1ae205f4528ba3d36e79cbb046d
JamesKemp296 <jameskemp2796@yahoo.com>
2019-12-23 23:47:26 -0500

    gradient over project preview

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import Image from 'gatsby-image'
+import React from "react"
+import { Link } from "gatsby"
+import Image from "gatsby-image"
 
-import './project.css'
-const ProjectPreview = ({ slug, title, description, imageData, }) => (
+import "./project.css"
+const ProjectPreview = ({ slug, title, description, imageData, sub }) => (
   <div className="project-preview">
-    <div className="project-image grow">
+    <div className="project-image">
       <Link to={`/${slug}/`}>
-        <Image fluid={imageData} alt={title} className="preview-image-grow"/>
+        <Image fluid={imageData} alt={title} className="preview-image" />
       </Link>
+      <div className="overlay">
+        <div className="text">{sub}</div>
+      </div>
     </div>
     <div className="projects-text">
       <h2>
