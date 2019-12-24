3c7af37bcc65861317ebeeca1202c4e4c7ad23eb
JamesKemp296 <jameskemp2796@yahoo.com>
2019-12-24 00:19:17 -0500

    fixed preview links

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -5,14 +5,15 @@ import Image from "gatsby-image"
 import "./project.css"
 const ProjectPreview = ({ slug, title, description, imageData, sub }) => (
   <div className="project-preview">
-    <div className="project-image">
-      <Link to={`/${slug}/`}>
+    <Link to={`/${slug}/`}>
+      <div className="project-image">
         <Image fluid={imageData} alt={title} className="preview-image" />
-      </Link>
-      <div className="overlay">
-        <div className="text">{sub}</div>
+
+        <div className="overlay">
+          <div className="text">{sub}</div>
+        </div>
       </div>
-    </div>
+    </Link>
     <div className="projects-text">
       <h2>
         <Link to={`/${slug}/`}>{title}</Link>
