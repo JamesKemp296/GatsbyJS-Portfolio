c0fe3bc0ba81a675de0f7ab06a004a1bef1fc516
JamesKemp296 <jameskemp2796@yahoo.com>
2020-01-24 13:34:43 -0500

    card animation is much smoother

--- src/components/GithubRepoCard/GithubRepoCard.js
+++ src/components/GithubRepoCard/GithubRepoCard.js
@@ -11,22 +11,24 @@ const GithubRepoCard = ({
 }) => {
   return (
     <a href={url} target="_blank" rel="noopener noreferrer" key={id}>
-      <div className="github-card shadow-pop-tr">
-        <h5>{name}</h5>
-        <p className="github-text">{language || "CSS"}</p>
-        <div id="github-dates">
-          <p className="github-text">
-            Created:{" "}
-            {moment(created_at)
-              .startOf("day")
-              .fromNow()}
-          </p>
-          <p className="github-text">
-            Updated:{" "}
-            {moment(updated_at)
-              .startOf("day")
-              .fromNow()}
-          </p>
+      <div className="card-wrapper">
+        <div className="github-card card-inner">
+          <h5>{name}</h5>
+          <p className="github-text">{language || "CSS"}</p>
+          <div id="github-dates">
+            <p className="github-text">
+              Created:{" "}
+              {moment(created_at)
+                .startOf("day")
+                .fromNow()}
+            </p>
+            <p className="github-text">
+              Updated:{" "}
+              {moment(updated_at)
+                .startOf("day")
+                .fromNow()}
+            </p>
+          </div>
         </div>
       </div>
     </a>
